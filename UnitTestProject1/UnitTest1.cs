1b79e7f26e90006e7e34eb716cdf7b19b1626104
Happymilk <happymilk853@gmail.com>
2016-03-18 20:55:08 +0300

    Tests without bodys

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestCase
+    {
+        #region Inequilateral //неравносторонний
+        [TestMethod]
+        public void Positive_Inequilateral()
+        {
+        }
+        [TestMethod]
+        public void Negative_Inequilateral()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_NonexistentInequilateral()
+        {
+        }
+        [TestMethod]
+        public void Negative_NonexistentInequilateral()
+        {
+        }
+        #endregion 
+
+        #region Equilateral //равносторонний
+        [TestMethod]
+        public void Positive_Equilateral()
+        {
+        }
+        [TestMethod]
+        public void Negative_Equilateral()
+        {
+        }
+        #endregion 
+
+        #region Isosceles //равнобедренный
+        [TestMethod]
+        public void Positive_Isosceles()
+        {
+        }
+        [TestMethod]
+        public void Negative_Isosceles()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_NonexistentIsosceles()
+        {
+        }
+        [TestMethod]
+        public void Negative_NonexistentIsosceles()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_CorrectIsosceles1()
+        {
+        }
+        [TestMethod]
+        public void Negative_CorrectIsosceles1()
+        {
+        }
+        [TestMethod]
+        public void Positive_CorrectIsosceles2()
+        {
+        }
+        [TestMethod]
+        public void Negative_CorrectIsosceles2()
+        {
+        }
+        [TestMethod]
+        public void Positive_CorrectIsosceles3()
+        {
+        }
+        [TestMethod]
+        public void Negative_CorrectIsosceles3()
+        {
+        }
+        #endregion
+
+        #region Sides //стороны
+        [TestMethod]
+        public void Positive_NullSide()
+        {
+        }
+        [TestMethod]
+        public void Negative_NullSide()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_NegativeSide()
+        {
+        }
+        [TestMethod]
+        public void Negative_NegativeSide()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_SumOfTwo()
+        {
+        }
+        [TestMethod]
+        public void Negative_SumOfTwo()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_FirstEqualOthers1()
+        {
+        }
+        [TestMethod]
+        public void Negative_FirstEqualOthers1()
+        {
+        }
+        [TestMethod]
+        public void Positive_FirstEqualOthers2()
+        {
+        }
+        [TestMethod]
+        public void Negative_FirstEqualOthers2()
+        {
+        }
+        [TestMethod]
+        public void Positive_FirstEqualOthers3()
+        {
+        }
+        [TestMethod]
+        public void Negative_FirstEqualOthers3()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_SumOfTwoLessThenThird()
+        {
+        }
+        [TestMethod]
+        public void Negative_SumOfTwoLessThenThird()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_ThreeTranspositions1()
+        {
+        }
+        [TestMethod]
+        public void Negative_ThreeTranspositions1()
+        {
+        }
+        [TestMethod]
+        public void Positive_ThreeTranspositions2()
+        {
+        }
+        [TestMethod]
+        public void Negative_ThreeTranspositions2()
+        {
+        }
+        [TestMethod]
+        public void Positive_ThreeTranspositions3()
+        {
+        }
+        [TestMethod]
+        public void Negative_ThreeTranspositions3()
+        {
+        }
+
+        [TestMethod]
+        public void Positive_NullSides()
+        {
+        }
+        [TestMethod]
+        public void Negative_NullSides()
+        {
+        }
+        #endregion
+
+        #region Values //значения
+        [TestMethod]
+        public void Positive_DoubleValues()
+        {
+        }
+        [TestMethod]
+        public void Negative_DoubleValues()
+        {
+        }
+        [TestMethod]
+        public void Positive_NotAllValues()
+        {
+        }
+        [TestMethod]
+        public void Negative_NotAllValues()
+        {
+        }
+        #endregion
+
+        #region Borders //границы
+        [TestMethod]
+        public void Positive_MaxBorder()
+        {
+        }
+        [TestMethod]
+        public void Negative_MaxBorder()
+        {
+        }
+        
+        [TestMethod]
+        public void Positive_MinBorder()
+        {
+        }
+        [TestMethod]
+        public void Negative_MinBorder()
+        {
+        }
+        #endregion
+
+        #region Separator 
+        [TestMethod]
+        public void o_____________________________________o()
+        {
+        }
+        #endregion
+    }
+}
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,20 +10,30 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
+        private bool Check(double a, double b, double c)
+        {
+            if ((a < b + c) && (b < a + c) && (c < a + b))
+                return true;
+            return false;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             double firstValue, secondValue, thirdValue;
 
-            firstValue = (double)numericUpDown1.Value;
+            firstValue  = (double)numericUpDown1.Value;
             secondValue = (double)numericUpDown2.Value;
-            thirdValue = (double)numericUpDown3.Value;
+            thirdValue  = (double)numericUpDown3.Value;
 
-            if (firstValue == secondValue && firstValue == thirdValue && secondValue == thirdValue)
-                label5.Text = "Треугольник равносторонний";
-            else if (firstValue == secondValue || firstValue == thirdValue || secondValue == thirdValue)
-                label5.Text = "Треугольник равнобедренный";
+            if (Check(firstValue, secondValue, thirdValue))
+                if (firstValue == secondValue && firstValue == thirdValue && secondValue == thirdValue)
+                    label5.Text = "Треугольник равносторонний";
+                else if (firstValue == secondValue || firstValue == thirdValue || secondValue == thirdValue)
+                    label5.Text = "Треугольник равнобедренный";
+                else
+                    label5.Text = "Треугольник неравносторонний";
             else
-                label5.Text = "Треугольник неравносторонний";
+                label5.Text = "Треугольник не существует";
 
             listBox1.Items.Add(numericUpDown1.Value + " " + numericUpDown2.Value + " " + numericUpDown3.Value + " " + label5.Text);  
         }
